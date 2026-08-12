1a947f676677dc578f4e82050cd2f71bfab7d380
1-J-1 <nowijnah15@gmail.com>
2026-08-12 09:50:28 +0900

    query update

--- src/main/java/ajou/aim_be/keyword/repository/PostKeywordRepository.java
+++ src/main/java/ajou/aim_be/keyword/repository/PostKeywordRepository.java
@@ -2,6 +2,8 @@ package ajou.aim_be.keyword.repository;
 
 import ajou.aim_be.keyword.PostKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,5 +18,16 @@ public interface PostKeywordRepository extends JpaRepository<PostKeyword, Long> 
     List<PostKeyword> findByPost_PostIdInOrderByPost_PostIdAscPostKeywordIdAsc(List<Long> postIds);
 
     void deleteByKeyword_KeywordId(Long keywordId);
+
+    @Query("""
+        SELECT pk
+        FROM PostKeyword pk
+        JOIN FETCH pk.keyword
+        WHERE pk.post.postId IN :postIds
+        ORDER BY pk.post.postId ASC, pk.postKeywordId ASC
+    """)
+    List<PostKeyword> findAllWithKeywordByPostIds(
+            @Param("postIds") List<Long> postIds
+    );
 
 }
